c883173b151694dfde954821d05742a22706ba7e
Franko Vekić <franko.vekic@gmail.com>
2022-08-29 17:01:40 +0200

    Adding an Edit handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using Application;
 using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +25,14 @@ namespace API.Controllers
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
             return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            
+            return Ok(await Mediator.Send(new Edit.Command {Activity = activity}));
         }
     }
 }
