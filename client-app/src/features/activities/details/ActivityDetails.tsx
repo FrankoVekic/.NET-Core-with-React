c2fa893346276d4dc3fdf860e0cde0fb40cf5f5d
Franko Vekić <franko.vekic@gmail.com>
2022-09-06 20:55:06 +0200

    Creating an Activity form

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -10,15 +10,15 @@ export default function ActivityDetails ({activity}: Props)
 {
     return (
         <Card fluid>
-    <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
-    <Card.Content>
-      <Card.Header>{activity.title}</Card.Header>
-      <Card.Meta>
-        <span>{activity.date}</span>
-      </Card.Meta>
-      <Card.Description>
-        {activity.description}
-      </Card.Description>
+        <Image src={`/assets/categoryImages/${activity.category}.jpg`}/>
+            <Card.Content>
+                <Card.Header>{activity.title}</Card.Header>
+                    <Card.Meta>
+                        <span>{activity.date}</span>
+                    </Card.Meta>
+                <Card.Description>
+                {activity.description}
+            </Card.Description>
     </Card.Content>
     <Card.Content extra>
         <Button.Group widths='2'>
